082ba8e6cfe5931698dd26f4cb4dfbbdd7997b19
lijin-xa <641313281@qq.com>
2020-09-14 19:20:40 +0800

    test code

---
+++ one.go
@@ -0,0 +1,17 @@
+/**
+* 第一个简单go程序
+* 1 go mod init xx 适用7牛中国区代理 下载go依赖包 https://goproxy.cn,direct
+* File - Settings - Go - Go Modules 引用GOPROXY=https:\\goproxy.cn,direct
+* 来实现不用在指定目录下src编写项目
+* func 函数 import 外部引用
+* fmt.Println 输出
+*/
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("hello word")
+}
+
