e9980ca345ec11b4061a36da80b4533d16512e95
lijin-xa <641313281@qq.com>
2020-09-22 16:25:19 +0800

    调整内部代码格式

--- struct/oop.go
+++ struct/oop.go
@@ -11,7 +11,7 @@ import (
 * 但是通过结构体(struct)来实现面向对象编程
 * 结构体内嵌实现类的派生
 * 接收器(receiver)实现 - 结构体添加方法
-*/
+ */
 
 // 入口接口
 func StartOOP() {
@@ -58,12 +58,12 @@ func (v Vec2) DistanceTo(other Vec2) float32 {
 	dx := v.X - other.X
 	dy := v.Y - other.Y
 
-	return float32(math.Sqrt(float64(dx * dx + dy * dy)))
+	return float32(math.Sqrt(float64(dx*dx + dy*dy)))
 }
 
 // 单位化
 func (v Vec2) Normalize() Vec2 {
-	mag := v.X * v.X + v.Y * v.Y
+	mag := v.X*v.X + v.Y*v.Y
 	if mag > 0 {
 		oneOverMag := 1 / math.Sqrt(float64(mag))
 
@@ -90,7 +90,7 @@ func (m MyInt) Add(other int) int {
 
 // 测试基础类型添加方法
 func baseTypeAddMethod() {
-	var b MyInt  // 默认为0
+	var b MyInt // 默认为0
 	fmt.Println(b.IsZero())
 
 	b = 1
@@ -100,7 +100,6 @@ func baseTypeAddMethod() {
 // 无论是普通函数还是结构体方法，只要他们的签名一致
 // 就可以使用与它们签名一致的函数变量来保存普通函数和结构体的方法
 type class struct {
-
 }
 
 func (c *class) Do(v int) {
@@ -136,7 +135,6 @@ func variableInAssigment() {
 // 2 事件发起者，会根据注册信息通知这些注册者
 // 3 一个事件可以有多个实现方响应
 
-
 /*
 * 类型内嵌和结构体内嵌 - 模拟类的继承
 * 结构体内的匿名字段（结构体类型）- 结构体可以包含内嵌结构体
@@ -144,7 +142,7 @@ func variableInAssigment() {
 * 1 内嵌结构体可以直接访问其成员变量 - 无须一层层方法 可以直接在外层访问内层的字段
 * 需要注意的是 如果结构体内部存在重名字段 就必须一层一层访问赋值 避免歧义
 * 2 内嵌的结构体字段名就是它的类型名
-*/
+ */
 
 type innerS struct {
 	int1 int
@@ -152,14 +150,13 @@ type innerS struct {
 }
 
 type outerS struct {
-	 b int
-	 c float32
-	 int     // anonymous field
-	 innerS  // anonymous field
+	b      int
+	c      float32
+	int    // anonymous field
+	innerS // anonymous field
 }
 
 func innerStruct_Type() {
-
 	// 创建一个outerS的实例化
 	outer := new(outerS)
 	outer.b = 1
@@ -223,23 +220,23 @@ type Car struct {
 
 	// 内嵌结构化的声明在内部 在初始化时必须带入其声明字段
 	Engine struct {
-		Power int   // 功率
-		Type string // 类型
+		Power int    // 功率
+		Type  string // 类型
 	}
 }
 
-func InitStruct () {
-	c := Car {
-		Wheel: Wheel {
+func InitStruct() {
+	c := Car{
+		Wheel: Wheel{
 			Size: 1,
 		},
 
 		// 在初始化时还是必须带入声明时的定义
 		Engine: struct {
 			Power int
-			Type string
+			Type  string
 		}{
-			Type: "1.5T",  // 分隔符,必须加上
+			Type:  "1.5T", // 分隔符,必须加上
 			Power: 143,
 		},
 	}
