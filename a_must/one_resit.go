1b74942db6d6a62650cc7feffacfeff688cf13b1
lijin.xa <641313281@qq.com>
2020-09-26 22:39:37 +0800

    快速排序未修复完全

--- a_must/one_resit.go
+++ a_must/one_resit.go
@@ -31,10 +31,16 @@ func main() {
 	// 实现一个接口
 	//interfaceDemo()
 
-	//sliceDemo()
+	//numList := sliceDemo(10)
 
-	var number = []int{3, 1, 4, 2, 5}
-	quickSort(number)
+	var numList = []int{1, 7, 7, 9, 1, 8, 5, 0, 6, 0}
+	lower := 0
+	high := len(numList) - 1
+	var a = 0
+	count := &a
+
+	quickSort(numList, lower, high, count)
+	fmt.Println("排序后: ",numList)
 }
 
 func interfaceDemo() {
@@ -47,37 +53,141 @@ func interfaceDemo() {
 
 // 切片的熟练掌握
 // 声明一个100长度的切片
-func sliceDemo() {
+func sliceDemo(length int) []int {
 	//var numList = []int{}
 	// 动态创建一个切片
-	var numList = make([]int, 100)
+	var numList = make([]int, length)
 
 	// 插入100个元素
-	for i := 0; i < 100; i++ {
-		numList[i] = rand.Intn(100)
+	for i := 0; i < length; i++ {
+		numList[i] = rand.Intn(length)
 	}
 
 	fmt.Println(len(numList))
+
+	return numList
 }
 
 // 快速排序
-func quickSort(numList []int) {
+func quickSort(numList []int, lower int, high int, count *int) {
+
+	comparePos := test1(numList, lower, high, count)
+
+	fmt.Println("the first comparePos: ", "lower: ", lower, " high: ", high,
+		" comparePos: ",  comparePos)
+
+	// 左端排序
+	if lower < comparePos {
+		//fmt.Println("lower: ", lower, " high: ", comparePos-1)
+		fmt.Println("左端数: ", numList[lower:comparePos-1])
+		quickSort(numList, lower, comparePos-1, count)
+	}
+	// 右端排序
+	if high > comparePos {
+		//fmt.Println("high: ", high, " lower: ", comparePos+1)
+		fmt.Println("右端数: ", numList[comparePos+1:high])
+		quickSort(numList, comparePos+1, high, count)
+	}
+}
+
+func test2(numList []int, lower int, high int, count *int) int {
+	*count = *count + 1
+	fmt.Println("----------- ", *count," -----------")
+
+	// 比较位
+	var compareV = numList[lower]
+	var comparePos = lower
+
+	fmt.Println("start", numList,
+		"comparePos: ", comparePos, " compareV: ", compareV)
+
+	for i := high; i > lower; i-- {
+		// 小于放左端
+		if i > comparePos && numList[i] <= compareV {
+
+			fmt.Println("compareV[",comparePos, "]:", compareV,
+				"[", i , "]: ", numList[i],)
+
+			// 和比较位交换
+			numList[comparePos], numList[i] = numList[i], numList[comparePos]
+			// 修改比较位
+			comparePos = i
+
+			fmt.Println("交换后: ", numList)
+
+			//lower := numList[i]
+			// 先将这个数 从切片中删除
+			//numList = append(numList[:i], numList[i+1:]...)
+			// 再插入到第一个位置
+
+		} else if i < comparePos && numList[i] > compareV {
+			fmt.Println("[", i ,"]:", numList[i],
+				"compareV[:",comparePos,"]", compareV)
+
+			// 和比较位交换
+			numList[comparePos], numList[i] = numList[i], numList[comparePos]
+			// 修改比较位
+			comparePos = i
+			fmt.Println(numList)
+		}
+	}
+	fmt.Println("end", numList, "numList[",comparePos,"]",compareV)
+
+	return comparePos
+}
+
+// 错误版本1
+func test1(numList []int, lower int, high int, count *int) int {
+	*count = *count + 1
+	fmt.Println("----------- ", *count," -----------")
 
 	// 步骤1
 	// 将 3 1 4 2 5  固定3  1 2 3 4 5
 	// 2 3 1 4 5
 
 	// 比较位
-	var compareV = numList[0]
-	for i := len(numList)-1; i > 0; i-- {
-		if numList[i] < compareV {
+	var compareV = numList[lower]
+	var comparePos = lower
+
+	fmt.Println("start", numList,
+		"comparePos: ", comparePos, " compareV: ", compareV)
+
+	for i := high; i > lower; i-- {
+		// 小于放左端
+		if i > comparePos && numList[i] <= compareV {
+
+			fmt.Println("compareV[",comparePos, "]:", compareV,
+			"[", i , "]: ", numList[i],)
+
+			// 和比较位交换
+			numList[comparePos], numList[i] = numList[i], numList[comparePos]
+			// 修改比较位
+			comparePos = i
+
+			fmt.Println("交换后: ", numList)
+
 			//lower := numList[i]
 			// 先将这个数 从切片中删除
 			//numList = append(numList[:i], numList[i+1:]...)
-
 			// 再插入到第一个位置
+
+		} else if i < comparePos && numList[i] > compareV {
+			fmt.Println("[", i ,"]:", numList[i],
+				"compareV[:",comparePos,"]", compareV)
+
+			// 和比较位交换
+			numList[comparePos], numList[i] = numList[i], numList[comparePos]
+			// 修改比较位
+			comparePos = i
+			fmt.Println(numList)
 		}
 	}
+	fmt.Println("end", numList, "numList[",comparePos,"]",compareV)
+	//*comparePos1 = comparePos
+
+	return comparePos
+
+	//result[comparePos] = compareV
 }
 
 //type error interface {
