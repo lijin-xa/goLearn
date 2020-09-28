29b85529df13edbf158dde70389f3a0cb58d278c
lijin-xa <641313281@qq.com>
2020-09-28 19:36:43 +0800

    使用mongo-driver完成简单的增删改查

--- mongodb/base.go
+++ mongodb/base.go
@@ -11,36 +11,151 @@ import (
 
 // 使用MongoDB Go Driver
 // MongoDB所采用的数据格式被称为BSON - 基于JSON的二进制序列化格式
+// Go 使用BSON对象
+// Go Driver 有两个系列表示BSON D系列 Raw系列
+// D类型有4种类型
+// D - BSON文档
+// M - Map
+// A - 数组
+// E - 在D里面的一个单一的子项
 // 用于存储文档并进行远程过程调用
+var Client *mongo.Client
+
+// context - 传递和请求具有生命周期的变量的标准方法
+// 程序单元的一个运行状态，现场，快照
+var collection *mongo.Collection
+var ctx context.Context
 
 func Start() {
 	fmt.Println("Start connect")
 
 	// 使用Go Driver连接到MongoDB
-
-	// 连接选项 URI不需要IP地址 直接使用端口号即可
+	// 配置driver的设定 write concern 错误捕获机制
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	// 连接到MongoDB
+	// 连接到MongoDB 必须传递两个参数 context 和 options.ClientOptions对象
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("connect failed, err%s\n", err)
+		return
+		//log.Fatal(err)
 	}
 
-	// check the connection
+	// 测试连接
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("client Ping failed, err%s\n", err)
+		return
+		//log.Fatal(err)
 	}
-
+	Client = client
 	fmt.Println("Connected to MongoDB")
 
-	OperateTest(client)
+	OperateDemo()
 }
 
+// 基础操作练习
+func OperateDemo() {
+	// 返回一个操作集合的句柄
+	collection = Client.Database("test").Collection("temp")
+	fmt.Println("connect collection name:", collection.Name())
+
+	//insertDemo()
+	//findDemo()
+	//deleteDemo()
+	updateDemo()
+}
+
+type user struct {
+	Id   int    `bson:"_id"`
+	Name string `bson:"name"`
+}
+
+// 查询集合文档
+func findDemo() {
+	// 需要过滤文档
+	filter := bson.M{"_id": 1}
+	result := collection.FindOne(ctx, filter)
+
+	msg := &user{}
+	// 结果的解码
+	err := result.Decode(msg)
+	if err != nil {
+		fmt.Printf("find one failed, err %s\n", err)
+	}
+	fmt.Printf("_id: %d, name: %s\n", msg.Id, msg.Name)
+
+	// TODO 多条数据查找 暂时还存在问题
+	filter = bson.M{"[key]": "[value]"}
+	collection.Find(ctx, filter)
+}
+
+// 插入文档练习
+func insertDemo() {
+	// map类型
+	b := bson.M{
+		"_id":  1,
+		"name": "lijin",
+	}
+	// 插入bson数据不能有_id字段
+	_, err := collection.InsertOne(ctx, b)
+	if err != nil {
+		fmt.Printf("insert data failed, err%s\n", err)
+		//return
+	}
+	// fmt.Printf("insert id %v\n", result.InsertedID)
+
+	// map切片
+	bsonList := []interface{}{
+		bson.M{
+			"_id":  2,
+			"name": "go",
+		},
+		bson.M{
+			"_id":  3,
+			"name": "lua",
+		},
+	}
+	// 插入多条数据
+	res, err := collection.InsertMany(ctx, bsonList)
+	if err != nil {
+		fmt.Printf("InsertMany failed, err%s\n", err)
+		return
+	}
+	fmt.Printf("InsertMany ids %v\n", res.InsertedIDs)
+}
+
+// 删除练习
+func deleteDemo() {
+	// 删除一条数据
+
+	filter := bson.M{"_id": 3}
+	result, err := collection.DeleteOne(ctx, filter)
+	if err != nil {
+		fmt.Printf("delete failed, err%s\n", err)
+		return
+	}
+	fmt.Println("result.DeletedCount", result.DeletedCount)
+}
+
+// 修改文档练习
+func updateDemo() {
+	filter := bson.M{"_id": "2"}
+	update := bson.M{"name": "go"}
+
+	// TODO update bson 暂时还有些问题
+	// mongo.UpdateResult
+	result, err := collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		fmt.Printf("update failed, err %s\n", err)
+	}
+	fmt.Println("ModifiedCount: ", result.ModifiedCount)
+}
+
+// 以下老代码
 type Trainer struct {
 	Name string
-	Age int
+	Age  int
 	City string
 }
 
@@ -58,7 +173,7 @@ func OperateTest(client *mongo.Client) {
 	fmt.Println("insert a single document Id:", insertResult.InsertedID)
 
 	// 更新文档
-	filter := bson.D{{"name", "Jam"}}  // 匹配过滤器
+	filter := bson.D{{"name", "Jam"}} // 匹配过滤器
 	update := bson.D{
 		{"$inc", bson.D{
 			{"age", 1},
@@ -73,7 +188,10 @@ func OperateTest(client *mongo.Client) {
 
 	// 查找文档
 	var checkResult Trainer
-	err = collection.FindOne(context.TODO(), filter). Decode(&checkResult)
+
+	// 错误记录argument to Decode must be a pointer or a map, but got { 0 }
+	// Decode参数必须是一个指针或者map
+	err = collection.FindOne(context.TODO(), filter).Decode(&checkResult)
 	if err != nil {
 		log.Fatal(err)
 	}
